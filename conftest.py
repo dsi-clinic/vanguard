81caf806d94b781855972cac6b9850588910680d
Jim Pivarski <jpivarski@gmail.com>
2026-01-06 17:32:35 -0600

    pre-commit

--- conftest.py
+++ conftest.py
@@ -1,3 +1,7 @@
-def pytest_sessionfinish(session, exitstatus):
-    if exitstatus == 5:
+"""Configure pytest."""
+
+
+def pytest_sessionfinish(session, exitstatus) -> None:  # noqa: ANN001
+    """Disable warnings-as-workflow errors; our upstream dependencies raise warnings."""
+    if exitstatus == 5:  # noqa: PLR2004
         session.exitstatus = 0
